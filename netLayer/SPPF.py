c1b09d8c79c0bf94cce4f8d9d0bd649959a0de46
100oylz <2662317636@qq.com>
2023-11-11 09:43:43 +0800

    init SPPF

---
+++ netLayer/SPPF.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from ConvModule import ConvModule
+
+
+class SPPF(nn.Module):
+    def __init__(self, in_channel: int, out_channel: int, kernel_size=5):
+        """
+        SPPF
+        :param in_channel: 输入通道数
+        :type in_channel: int
+        :param out_channel: 输出通道数
+        :type out_channel: int
+        :param kernel_size: 最大池化时的kernel_size
+        :type kernel_size: int
+        """
+        super().__init__()
+        hidden_channel = in_channel // 2
+        self.conv_1 = ConvModule(
+            in_channel=in_channel,
+            out_channel=hidden_channel,
+            kernel_size=1, stride=1,
+            padding=0)
+        self.conv_2 = ConvModule(
+            in_channel=hidden_channel * 4,
+            out_channel=out_channel,
+            kernel_size=1, stride=1,
+            padding=0)
+        self.maxpool2d = nn.MaxPool2d(kernel_size=5, stride=1, padding=kernel_size // 2)
+
+    def forward(self, x):
+        x = self.conv_1(x)
+        y1 = self.maxpool2d(x)
+        y2 = self.maxpool2d(y1)
+        y3 = self.maxpool2d(y2)
+        out = self.conv_2(torch.cat((x, y1, y2, y3), 1))
+        return out
