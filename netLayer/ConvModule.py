105abd12bd7ffeb0c37f5996ab47490552b54c51
100oylz <2662317636@qq.com>
2023-11-11 09:25:53 +0800

    init ConvModule

---
+++ netLayer/ConvModule.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ConvModule(nn.Module):
+    def __init__(self, kernel_size: int, stride: int, padding: int, in_channel: int, out_channel: int):
+        """
+        定义了ConvModule
+        :param kernel_size: 卷积核大小
+        :type kernel_size: int
+        :param stride: 步长
+        :type stride: int
+        :param padding: 填充宽度
+        :type padding: int
+        :param in_channel: 输入通道数
+        :type in_channel: int
+        :param out_channel: 输出通道数
+        :type out_channel: int
+        """
+        super().__init__()
+        self.conv_module_net = nn.Sequential(
+            nn.Conv2d(in_channels=in_channel, out_channels=out_channel, kernel_size=kernel_size, stride=stride,
+                      padding=padding),
+            nn.BatchNorm2d(num_features=out_channel),
+            nn.SiLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.conv_module_net(x)
