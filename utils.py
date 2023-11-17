98b9758c2b589e05131f3b0a297947d2df56698e
100oylz <2662317636@qq.com>
2023-11-17 10:15:53 +0800

    add 转化图像为tensor的方法，并且返回图像原始大小的列表

--- utils.py
+++ utils.py
@@ -2,9 +2,12 @@ import os
 import cv2
 import pandas as pd
 import matplotlib.pyplot as plt
+import torch
+
 import config
 from typing import List, Tuple
 import numpy as np
+from torchvision import transforms
 
 
 def load_id_list(filepath: str) -> List[str]:
@@ -21,7 +24,7 @@ def load_id_list(filepath: str) -> List[str]:
     return id_list
 
 
-def load_all(filepath: str,split:int=None) -> Tuple[List[np.ndarray], List[pd.DataFrame]]:
+def load_all(filepath: str, split: int = None) -> Tuple[List[np.ndarray], List[pd.DataFrame]]:
     """
     读取文件夹下全部数据
     :param filepath:文件夹路径
@@ -32,8 +35,8 @@ def load_all(filepath: str,split:int=None) -> Tuple[List[np.ndarray], List[pd.Da
     id_list = load_id_list(filepath)
     img_list = []
     csv_list = []
-    if(split!=None):
-        id_list=id_list[:split]
+    if (split != None):
+        id_list = id_list[:split]
     for fileid in id_list:
         image_path = os.path.join(config.DIRPATH, config.IMAGEFORMAT.format(fileid))
         csv_path = os.path.join(config.DIRPATH, config.CSVFORMAT.format(fileid))
@@ -50,6 +53,28 @@ def load_all(filepath: str,split:int=None) -> Tuple[List[np.ndarray], List[pd.Da
         csv_list.append(csv_data)
 
     return img_list, csv_list
+
+
+def transform_image_to_tensor(img_list: List[np.ndarray]) -> (List[torch.Tensor], List[Tuple[int]]):
+    """
+    将图像转化为transform后的tensor，并且返回tensor列表和原始图像大小
+    :param img_list: 图片列表
+    :type img_list: List[np.ndarray]
+    :return: 图像transform后的列表，图像原本的shape列表
+    :rtype: (List[torch.Tensor], List[Tuple[int]])
+    """
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((640, 640)),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    ])
+    img_tensors, origin_shape_list = [], []
+    for img in img_list:
+        assert type(img) == np.ndarray, f"{type(img)} != numpy.ndarray"
+        origin_shape_list.append(img.shape)
+        img_tensor = transform(img)
+        img_tensors.append(img_tensor)
+    return img_tensors, origin_shape_list
 
 
 if __name__ == '__main__':
