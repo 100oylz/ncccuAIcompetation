70a45093be06bb6202f11b0cc55311f3887f90d1
100oylz <2662317636@qq.com>
2023-11-19 22:06:42 +0800

    data augment

--- project/model_predict.py
+++ project/model_predict.py
@@ -1,25 +1,26 @@
 from ultralytics import YOLO
 import torch
-modelpath = './best.pt'
+
+modelpath = './best.onnx'
 
 
 class predict():
     def __init__(self):
-        self.model = YOLO(modelpath)
+        self.model = YOLO(modelpath, task='detect')
 
     def detect_image(self, image_path):
         results = self.model(image_path, stream=True, device='cpu')
+        boxresults = []
         for result in results:
             boxes = result.boxes  # Boxes object for bbox outputs
-        results = []
-        for cls, conf, xyxy in zip(boxes.cls, boxes.conf, boxes.xyxy):
-            results.append([xyxy[1].item(), xyxy[0].item(), xyxy[3].item(), xyxy[2].item(), conf.item(), cls.item()])
+            for cls, conf, xyxy in zip(boxes.cls, boxes.conf, boxes.xyxy):
+                boxresults.append(
+                    [xyxy[1].item(), xyxy[0].item(), xyxy[3].item(), xyxy[2].item(), conf.item(), cls.item()])
 
-        return results
+        return boxresults
 
 
 if __name__ == '__main__':
-
     model = predict()
     print(model.detect_image(
-        r"D:\ProgramProject\PycharmProject\ObjectDetection_YOLO\dataset\images\val\0b1e7df6cc0e00d37ca7d16f14529304.jpg"))
+        r"D:\ProgramProject\PycharmProject\ObjectDetection_YOLO\dataset\images\val\0c842b1611a496b84c93716362f20482.jpg"))
